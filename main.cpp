7abe98cc8bf50c90062460d720b37d90608e5e48
Neel Mittal <130887636+neel2903@users.noreply.github.com>
2024-07-27 04:13:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-/** Global Constants **/
+
 const string TRACE = "trace";
 const string SHOW_STATISTICS = "stats";
 const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
